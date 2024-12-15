7694cb26081df6d82185fcea69b389230f30cd6e
Alvin Amisi <alvomakaya@gmail.com>
2024-12-15 20:37:24 +0300

    event listener

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+ // Step 1: Select the element
+ const input = document.getElementById('button');
+  
+ // Step 2: Define a callback function
+ function clickAlert() {
+   alert('I was clicked!');
+ }
+ 
+ // Step 3: Attach the event listener
+ input.addEventListener('click', clickAlert);
 }
